0761fe795a98d9cb876b488801485b4bf804ca4e
Valentin <navshop@ukr.net>
2016-07-24 16:26:11 +0300

    added changing background

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@
 
 	const $ = (selector) => document.querySelector(selector)
 
+	let $body = $('body')
 	let $main = $('.main')
 	let $design = $('.design')
 	let $develop = $('.develop')
@@ -14,6 +15,8 @@
 	let $seo = $('.seo')
 	let $support = $('.support')
 	let $project = $('.project')
+
+	console.log($body)
 
 	// variables of templates
 
@@ -27,34 +30,44 @@
 	let $navLinks = document.querySelectorAll('.navLink')
 
 	for (let $navLink of $navLinks) {
-		$navLink.addEventListener('click', () =>
+
+		console.log($navLink)
+		$navLink.addEventListener('click', (event) =>
 		{	
 			let {target} = event
 
-			console.log(target.classList[0])
+			console.log($body.className)
 
 			switch (target.classList[0])
 			{
 				case 'design': 
-					$main.innerHTML = $tempDesign.innerHTML 
+					$main.innerHTML = $tempDesign.innerHTML
+					$body.className = "bg_design"
 					return
-				case 'develop': 
+				case 'develop':
 					$main.innerHTML = $tempDevelop.innerHTML
+					$body.className = "bg_develop"
 					return
 				case 'tech': 
 					$main.innerHTML = $tempTech.innerHTML
+					$body.className = "bg_tech"
 					return
 				case 'seo': 
-					$main.innerHTML = $TempSeo.innerHTML
+					$main.innerHTML = $tempSeo.innerHTML
+					$body.className = "bg_seo"
 					return
 				case 'support': 
-					$main.innerHTML = $TempSupport.innerHTML
+					$main.innerHTML = $tempSupport.innerHTML
+					$body.className = "bg_support"
 					return
 				case 'project': 
-					$main.innerHTML = $TempProject.innerHTML
+					$main.innerHTML = $tempProject.innerHTML
+					$body.className = "bg_project"
 					return
 				default: console.log('Ups. Error in nav!!')
 			}
+			
+			event.preventDefault()
 		})
 	}
 })()
