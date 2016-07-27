7fa2ba905eb2dc7615f890cfee259ca311450dbe
Valentin Drany <navshop@ukr.net>
2016-07-27 15:13:46 +0300

    rewrite file
    add life nav menu

--- src/js/index.js
+++ src/js/index.js
@@ -5,69 +5,22 @@
 	NodeList.prototype[Symbol.iterator] = [][Symbol.iterator]
 	console.log("It's ok!!!")
 
-	const $ = (selector) => document.querySelector(selector)
+	let $ = (selector) => document.querySelector(selector)
+	let $All = (selector) => document.querySelectorAll(selector)
 
-	let $body = $('body')
-	let $main = $('.main')
-	let $design = $('.design')
-	let $develop = $('.develop')
-	let $tech = $('.tech')
-	let $seo = $('.seo')
-	let $support = $('.support')
-	let $project = $('.project')
+	if ( location.pathname == '/design_.html' )
+		$('.design').className = $('.design').className.concat(' selected')
+	if ( location.pathname == '/develop_.html' )
+		$('.develop').className = $('.develop').className.concat(' selected')
+	if ( location.pathname == '/tech_.html' )
+		$('.tech').className = $('.tech').className.concat(' selected')
+	if ( location.pathname == '/seo_.html' )
+		$('.seo').className = $('.seo').className.concat(' selected')
+	if ( location.pathname == '/support_.html' )
+		$('.support').className = $('.support').className.concat(' selected')
+	if ( location.pathname == '/projects_.html' )
+		$('.project').className = $('.project').className.concat(' selected')
+	if ( location.pathname == '/contacts_.html' )
+		$('.contacts').className = $('.contacts').className.concat(' selected')
 
-	console.log($body)
-
-	// variables of templates
-
-	let $tempDesign = $('.windowDesign')
-	let $tempDevelop = $('.windowDevelop')
-	let $tempTech = $('.windowTech')
-	let $tempSeo = $('.windowSeo')
-	let $tempSupport = $('.windowSupport')
-	let $tempProject = $('.windowProject')	
-	
-	let $navLinks = document.querySelectorAll('.navLink')
-
-	for (let $navLink of $navLinks) {
-
-		console.log($navLink)
-		$navLink.addEventListener('click', (event) =>
-		{	
-			let {target} = event
-
-			console.log($body.className)
-
-			switch (target.classList[0])
-			{
-				case 'design': 
-					$main.innerHTML = $tempDesign.innerHTML
-					$body.className = "bg_design"
-					break
-				case 'develop':
-					$main.innerHTML = $tempDevelop.innerHTML
-					$body.className = "bg_develop"
-					break
-				case 'tech': 
-					$main.innerHTML = $tempTech.innerHTML
-					$body.className = "bg_tech"
-					break
-				case 'seo': 
-					$main.innerHTML = $tempSeo.innerHTML
-					$body.className = "bg_seo"
-					break
-				case 'support': 
-					$main.innerHTML = $tempSupport.innerHTML
-					$body.className = "bg_support"
-					break
-				case 'project': 
-					$main.innerHTML = $tempProject.innerHTML
-					$body.className = "bg_project"
-					break
-				default: console.log('Ups. Error in nav!!')
-			}
-			console.log(event.preventDefault())
-			event.preventDefault()
-		})
-	}
 })()
