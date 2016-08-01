340f70f5832d17ab914d496f46d2b2562fab26b6
Valentin Drany <navshop@ukr.net>
2016-08-01 10:41:25 +0300

    update
    for icon id

--- src/js/index.js
+++ src/js/index.js
@@ -8,51 +8,19 @@
 	let $ = (selector) => document.querySelector(selector)
 	let $All = (selector) => document.querySelectorAll(selector)
 
-	let svgIcon = $('.aside').querySelector('svg')
-	let g1 = svgIcon.querySelector("g")
-	let g2 = svgIcon.nextElementSibling.querySelector("g")
-	let g3 = svgIcon.nextElementSibling
-					.nextElementSibling
-					.querySelector("g")
-	let g4 = svgIcon.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.querySelector("g")
-	let g5 = svgIcon.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.querySelector("g")					
-	let g6 = svgIcon.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.querySelector("g")
-	let g7 = svgIcon.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.querySelector("g")
-	let g8 = svgIcon.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.querySelector("g")
-	let g9 = svgIcon.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.nextElementSibling
-					.querySelector("g")
+	
+	function idIcon()
+	{	
+		let $svgs = $('.aside').querySelectorAll('svg')
+		let g = []
+		let index = 0
+
+		for ( let $svg of $svgs )
+		{
+			g[index] = $svg.querySelector("g")
+			g[index++].setAttribute("id", "icon" + index)
+		}
+	}
 
 	if ( location.pathname == '/design_.html' )
 		$('.design').className = $('.design').className.concat(' selected')
@@ -61,15 +29,7 @@
 	if ( location.pathname == '/tech_.html' )
 	{
 		$('.tech').className = $('.tech').className.concat(' selected')
-		g1.setAttribute("id", "icon1")
-		g2.setAttribute("id", "icon2")
-		g3.setAttribute("id", "icon3")
-		g4.setAttribute("id", "icon4")
-		g5.setAttribute("id", "icon5")
-		g6.setAttribute("id", "icon6")
-		g7.setAttribute("id", "icon7")
-		g8.setAttribute("id", "icon8")
-		g9.setAttribute("id", "icon9")
+		idIcon()
 	}
 	if ( location.pathname == '/seo_.html' )
 		$('.seo').className = $('.seo').className.concat(' selected')
